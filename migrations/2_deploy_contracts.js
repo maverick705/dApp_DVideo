a8b5c3835a0037f743d6d8d115ba0be84f73cf42
maverick705 <reiners88@gmail.com>
2020-12-14 06:51:28 -0500

    created DApp Template

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const _ = artifacts.require("_");
+
+module.exports = function (deployer) {
+  deployer.deploy(_);
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import _img_ from './src'
+
+export default function Navbar() {
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
+        <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="kenreinersdev.com"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <img src={_img_}/>
+        </a>
+
+    </nav>
+}
